f0c561a1576ebf4db0bbe2d0ff8b1141e03779c1
Sahil Sharma <sahilsharma21062000@gmail.com>
2024-07-14 21:45:00 +0200

    MasRCNN inference added

--- src/models/MaskRCNN/inference.py
+++ src/models/MaskRCNN/inference.py
@@ -1,36 +1,40 @@
+import os
+import argparse
 import torch
+from tqdm import tqdm
 import numpy as np
-import albumentations as A
+from torchvision.transforms import functional as F
 from PIL import Image
-import cv2
+from albumentations import Compose, Normalize
 from albumentations.pytorch import ToTensorV2
-from model import instance_model
-import matplotlib.pyplot as plt
+from model import get_model_instance_segmentation
+import cv2
 
 
 def load_model(model_path, num_classes, device):
-    model = instance_model(num_classes)
+    model = get_model_instance_segmentation(num_classes)
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     model.eval()
     return model
 
 
-def run_inference(model, device, image_path, threshold=0.5):
+def preprocess_image(image_path, device):
     # Define the transformation pipeline for inference
-    transform = A.Compose(
-        [
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            ToTensorV2(),
-        ]
+    transform = Compose(
+        [Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)), ToTensorV2()]
     )
 
     # Load and preprocess the input image using Albumentations
     img = Image.open(image_path).convert("RGB")
     img = np.array(img)
     transformed = transform(image=img)
-    img_tensor = transformed["image"]
-    img_tensor = img_tensor.unsqueeze(0).to(device)
+    img_tensor = transformed["image"].unsqueeze(0).to(device)
+    return img_tensor
+
+
+def run_inference(model, device, image_path, threshold=0.5):
+    img_tensor = preprocess_image(image_path, device)
 
     # Perform inference
     with torch.no_grad():
@@ -46,19 +50,17 @@ def run_inference(model, device, image_path, threshold=0.5):
     pred_masks = pred_masks[pred_scores >= threshold]
     pred_scores = pred_scores[pred_scores >= threshold]
 
-    return img, pred_boxes, pred_masks, pred_scores
+    return pred_boxes, pred_masks, pred_scores
 
 
-def visualize_results(img, boxes, masks, scores, threshold=0.7):
+def visualize_results(img, boxes, masks, scores, threshold=0.5):
     img_np = np.array(img)
 
     for i, box in enumerate(boxes):
         if scores[i] >= threshold:
             xmin, ymin, xmax, ymax = box.astype(int)
-
             # Draw the rectangle
             cv2.rectangle(img_np, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
-
             # Apply the mask
             mask = masks[i, 0] > 0
             img_np[mask] = [255, 0, 0]  # Red color for mask
@@ -66,44 +68,59 @@ def visualize_results(img, boxes, masks, scores, threshold=0.7):
     return Image.fromarray(img_np)
 
 
-def display_results(dset, idx, image_path, predicted_mask):
-    plt.figure(figsize=(10, 5))
+def main():
+    # Define command line arguments
+    parser = argparse.ArgumentParser(description="Inference for Spine Segmentation")
+    parser.add_argument(
+        "--model_path", type=str, required=True, help="Path to the trained model"
+    )
+    parser.add_argument(
+        "--Validation_Folder",
+        type=str,
+        required=True,
+        help="Path to the validation folder",
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        required=True,
+        help="Path to save the output images and metrics",
+    )
+    args = parser.parse_args()
 
-    plt.subplot(1, 3, 1)
-    plt.imshow(Image.open(image_path))
-    plt.axis("off")
-    plt.title(f"{dset} Image [{idx}]")
+    # Set device and number of classes
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    num_classes = 2  # Background and spine
 
-    plt.subplot(1, 3, 2)
-    image_path = f"Dataset/DeepD3_{dset}/spine_images/spine_{idx}.png"
-    mask = Image.open(image_path).convert("L")
-    binary_mask = mask.point(lambda p: p > 0 and 255)
-    plt.imshow(binary_mask, cmap="gray", vmin=0, vmax=255)
-    plt.axis("off")
-    plt.title("Spines Mask")
+    # Load the model
+    model = load_model(args.model_path, num_classes, device)
 
-    plt.subplot(1, 3, 3)
-    plt.imshow(predicted_mask)
-    plt.axis("off")
-    plt.title("Predicted Mask")
-    plt.show()
+    # Initialize lists for storing results
+    predictions_list = []
+
+    # Process images and generate predictions
+    input_folder = os.path.join(args.Validation_Folder, "input_images")
+    output_folder = args.output_path
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Sort the files based on their numerical suffixes
+    input_images = sorted(os.listdir(input_folder))
+
+    # Add tqdm to display the progress bar
+    for input_image_name in tqdm(input_images, desc="Processing images"):
+        if input_image_name.endswith(".png"):
+            image_path = os.path.join(input_folder, input_image_name)
+            img = Image.open(image_path).convert("RGB")
+
+            boxes, masks, scores = run_inference(
+                model, device, image_path, threshold=0.5
+            )
+            predicted_mask = visualize_results(img, boxes, masks, scores, threshold=0.5)
+            predictions_list.append(predicted_mask)
+
+            # Save prediction mask
+            predicted_mask.save(os.path.join(output_folder, f"pred_{input_image_name}"))
 
 
-# Set device and number of classes
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-num_classes = 2  # Background and spine
-
-# Load the model
-model_path = "spines_model.pt"
-model = load_model(model_path, num_classes, device)
-# print('Model Loaded !!')
-
-# Perform inference
-idx = 15
-dset = "Training"
-image_path = f"Dataset/DeepD3_{dset}/input_images/image_{idx}.png"
-img, boxes, masks, scores = run_inference(model, device, image_path, threshold=0.5)
-predicted_mask = visualize_results(img, boxes, masks, scores, threshold=0.5)
-
-# Display the results
-display_results(dset, idx, image_path, predicted_mask)
+if __name__ == "__main__":
+    main()
